88754172c5320b2aa00be73737fa30610be7d2a1
arroway <stephanie@minet.net>
2011-12-30 15:43:06 +0100

    add file test for the Stats package

---
+++ test-Stats.pl
@@ -0,0 +1,71 @@
+#! /usr/bin/env perl;
+
+use Stats;
+use MyTests;
+use strict;
+use warnings;
+
+my $nbTests = 13;
+
+my $stats = Stats->new();
+my $t = MyTests->new($nbTests);
+
+print "***** Just after initializtion:\n";
+
+$t->test("getlogLines", $stats->getLogLines(), 0);
+$t->test("getNickList", $stats->getNickList(), "");
+if ($stats->getDate()){ 
+  $t->test("getDate", "not null", "not null"); } 
+else{ 
+  $t->test("getDate", "null", "not null"); }
+if ($stats->getTime()){
+  $t->test("getTime", "not null", "not null"); } 
+else{
+  $t->test("getTime", "null", "not null"); }
+$t->test("getLogTime", $stats->getLogTime(), 0);
+##5
+
+print "\n***** There is one new nickname.\n";
+
+$stats->addNick("arroway");
+$t->test("addNick: getNickList", $stats->getNickList(), "arroway ");
+
+#reset all, destruct User objects
+#$stats->resetAll();
+##6
+
+$stats->listNick("arroway_");
+$t->test("listNick with 1 arg: getNickList", $stats->getNickList(), "arroway arroway_ ");
+##7
+
+print "\n***** There are many new nicknames.\n";
+
+my @nicks = "lolbot you";
+$stats->listNick(@nicks);
+$t->test("listNick with several values: getNickList", $stats->getNickList(), "arroway arroway_ lolbot you ");
+##8
+
+
+print "\n***** How many lines are logged ?\n";
+
+my @cmd = (undef, "JOIN", "NICK", "PRIVMSG", "HELLO");
+my $count = 1;
+#XXX
+print "XXX: there are warnings with the case undef\n\n";
+foreach my $key (@cmd){
+ 
+    $stats->log($key);
+    if ($key eq "JOIN" or 
+        $key eq "NICK" or
+        $key eq "PRIVMSG"){
+
+        $t->test("getLogLines", $stats->getLogLines(), $count);
+        $count += 1;
+    }
+    else{
+      $t->test("getLogLines", $stats->getLogLines(), $count-1);
+    }
+}
+##13
+
+$t->myTestsDone();
