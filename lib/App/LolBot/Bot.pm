b7a9e3978b4fae2f6663e930512617746c279ecb
arroway <stephanie@minet.net>
2013-08-29 09:53:50 +0200

    fix biatch

--- lib/App/LolBot/Bot.pm
+++ lib/App/LolBot/Bot.pm
@@ -186,7 +186,7 @@ sub run {
         my $facepalm = $self->stats->print_facepalm($1) if $msg =~m/^:!facepalm$/;
         $self->send($facepalm, 'PRIVMSG') if ($facepalm);
         
-        $self->send(":biaaatch", 'PRIVMSG') if ($msg =~m/:p/);
+        $self->send(":biaaatch", 'PRIVMSG') if ($msg =~m/^:(.*):p(.*)$/);
 
         $self->stats->rec_stats($user_nick,$msg);
       }
